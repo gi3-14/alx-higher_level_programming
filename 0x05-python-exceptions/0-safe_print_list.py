c8269a24f86eaa2b9a3fab1b3f334f0642bfddd9
idehen osariemen <g.o.idehen@gmail.com>
2022-07-19 00:16:21 +0100

    fixed bugs

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+nes (11 sloc)  261 Bytes
+
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    total = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            total += 1
+        except IndexError:
+            break
+    print("")
+    return (total)
